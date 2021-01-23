854a55ade8a7520caa935f5a7f7b4836057cab68
Arturas1989 <76004308+Arturas1989@users.noreply.github.com>
2021-01-23 13:32:09 +0200

    Render socials #17

--- js/main.js
+++ js/main.js
@@ -1,1 +1,7 @@
-console.log('coming soon')
+
+import { renderSocials } from './components/renderSocials.js';
+import { socialsData } from './data/socialsData.js';
+
+const socialsHTML = renderSocials(socialsData);
+
+console.log(socialsHTML)
